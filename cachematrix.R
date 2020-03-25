c5c2b7b4ad4d02e735ed0167d064621abe7a30c9
777lucky777 <62024175+777lucky777@users.noreply.github.com>
2020-03-25 13:40:28 -0400

    Update cachematrix.R
    Updated code for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+ 
+## Together these functions use solve() to invert a matrix (given as input),cache the result in the parent
+## ... environment and outputs the result.  If the input matrix does not change on the following iteration
+## ... then the cached result is retrieved and returned without recalculating the result.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(solve) m <<- solve
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+  
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix returned by the above function. If the same matrix is input again for a
+## ... following iteration, it will retrieve the cached result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
